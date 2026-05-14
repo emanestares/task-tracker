17190fa8cfcb13bbccedd9cf0ede3bc5c41dabf8
RichardPajarillo <recpajarillo@mymail.mapua.edu.ph>
2026-05-14 10:29:02 +0800

    style: increase max width of content area in AppLayout

--- tasktracker-frontend/src/layouts/AppLayout.jsx
+++ tasktracker-frontend/src/layouts/AppLayout.jsx
@@ -11,7 +11,7 @@ export default function AppLayout() {
       <div className="flex flex-col flex-1 min-w-0 overflow-hidden">
         <Navbar onMenuClick={() => setSidebarOpen(true)} />
         <main className="flex-1 overflow-y-auto px-5 sm:px-7 py-6">
-          <div className="max-w-5xl mx-auto">
+          <div className="max-w-6xl mx-auto">
             <Outlet />
           </div>
         </main>
